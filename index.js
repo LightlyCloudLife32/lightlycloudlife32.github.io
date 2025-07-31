8ec940539ffcd294a4328364a9c7c66b18eb6d54
LightlyCloudLife32 <LeafDuvetPrivate@outlook.jp>
2025-07-31 21:16:43 +0900

    多分出来た

---
+++ index.js
@@ -0,0 +1,23 @@
+//ページの内容を取得
+const _fetch = async (url = '') => {
+  return await (await (await fetch(url)).text()).toString();
+}
+//HTMLElementに変換
+const _parse = (text = '') => {
+  return new DOMParser().parseFromString(text, 'text/html').body;
+}
+//本体
+const _main = async (txt, page = '') => {
+  txt.append(_parse(await _fetch(`${location.origin}/pages/${page}.html`)));
+}
+
+//読み込まれた時に実行
+onload = async () => {
+  const txt = document.getElementById('text');
+  const param = new URLSearchParams(location.search).get('page');
+  if(!param) {
+    location.replace(`${location.origin}/?page=main`);
+    return;
+  }
+  _main(txt, param);
+}
